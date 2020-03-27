287d91f4daedccaed02ee106e946ebba17a80ebc
Jackson Walters <jacksonwalters@gmail.com>
2020-03-27 02:03:34 -0400

    fixed similar method, added subtitle

--- similar.py
+++ similar.py
@@ -8,4 +8,5 @@ def cos_sim(vec1,vec2):
 def sim_words_from_vec(vocab,word_vec):
 	queries = [w for w in vocab if w.is_lower and w.prob >= -15]
 	by_similarity = sorted(queries, key=lambda w: cos_sim(w.vector,word_vec), reverse=True)
-	return by_similarity[:10]
+	sim_tokens = by_similarity[:10]
+	return [token.text for token in sim_words]
