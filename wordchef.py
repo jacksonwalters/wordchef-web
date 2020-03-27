fe9ed8c2d7d022aadb092d4c89f0c5edfb58527a
Jackson Walters <jacksonwalters@gmail.com>
2020-03-27 02:23:59 -0400

    add result from sim search, try larger vocab

--- wordchef.py
+++ wordchef.py
@@ -1,9 +1,11 @@
 import spacy
 from flask import Flask, render_template, flash, redirect
 from forms import RecipeForm
+from similar import sim_words_from_vec
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'play-it-as-it-lays'
-nlp = spacy.load('en_core_web_sm')
+#nlp = spacy.load('en_core_web_sm')
 
 @app.route("/wordchef/", methods=['GET','POST'])
 def recipe():
@@ -19,10 +21,15 @@ def recipe():
 		vec_sum = token1.vector + token2.vector
 		#look up synonym from vector sum
 		#for now, just return sum as string
-		sum_word = ','.join(vec_sum.astype(str))
+
+		#sum_word = ','.join(vec_sum.astype(str))
+		sum_word = ','.join(sim_words_from_vec(nlp.vocab,vec_sum))
+
+		#flash the result
 		flash('{}+{}={}'.format(word1, word2, sum_word))
 		return redirect('/wordchef')
 	return render_template('recipe.html', title='word+chef', form=form)
 
 if __name__ == "__main__":
+	nlp = spacy.load('en_core_web_md')
     app.run(host='0.0.0.0')
