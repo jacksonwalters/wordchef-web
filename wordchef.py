b7f59577eccde7f0b9ddbaa4e4ece6d4370905ce
Jackson Walters <jacksonwalters@gmail.com>
2020-03-27 00:10:49 -0400

    update

--- wordchef.py
+++ wordchef.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from forms import RecipeForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'play-it-as-it-lays'
@@ -6,6 +6,9 @@ app.config['SECRET_KEY'] = 'play-it-as-it-lays'
 @app.route("/wordchef/", methods=['GET','POST'])
 def recipe():
 	form = RecipeForm()
+	if form.validate_on_submit():
+		flash('word1={}, word2={}'.format(form.word1.data, form.word2.data))
+		return redirect('/wordchef')
 	return render_template('recipe.html', title='word+chef', form=form)
 
 if __name__ == "__main__":
