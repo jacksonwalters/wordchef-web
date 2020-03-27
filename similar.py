e8c4e136eda106363dcc9309eb78ceed703eea2d
Jackson Walters <jacksonwalters@gmail.com>
2020-03-27 02:12:05 -0400

    typo

--- similar.py
+++ similar.py
@@ -9,4 +9,4 @@ def sim_words_from_vec(vocab,word_vec):
 	queries = [w for w in vocab if w.is_lower and w.prob >= -15]
 	by_similarity = sorted(queries, key=lambda w: cos_sim(w.vector,word_vec), reverse=True)
 	sim_tokens = by_similarity[:10]
-	return [token.text for token in sim_words]
+	return [token.text for token in sim_tokens]
