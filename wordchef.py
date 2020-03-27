4c43ff974e7ed1219ccf9b48f71a60ca401eb862
Jackson Walters <jacksonwalters@gmail.com>
2020-03-27 09:27:15 -0400

    loading spacy

--- wordchef.py
+++ wordchef.py
@@ -5,31 +5,33 @@ from similar import sim_words_from_vec
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'play-it-as-it-lays'
-#nlp = spacy.load('en_core_web_sm')
 
 @app.route("/wordchef/", methods=['GET','POST'])
 def recipe():
 	form = RecipeForm()
 	if form.validate_on_submit():
 		#try loading larger spacy here
-		nlp = spacy.load('en_core_web_md')
+		try:
+			nlp
+		except NameError:
+			nlp = spacy.load('en_core_web_md')
+
 		#retrieve words from form
 		word1 = form.word1.data
 		word2 = form.word2.data
+
 		#use spacy to get tokens
 		token1 = [token for token in nlp(word1)][0]
 		token2 = [token for token in nlp(word2)][0]
+
 		#add the word vectors
 		vec_sum = token1.vector + token2.vector
-
-		#just return sum as string
-		#sum_word = ','.join(vec_sum.astype(str))
 
 		#look up synonym from vector sum
 		sum_word = ','.join(sim_words_from_vec(nlp.vocab,vec_sum))
 
 		#flash the result
-		flash('{}+{}={}'.format(word1, word2, sum_word))
+		flash('{}+{}~[{}]'.format(word1, word2, sum_word))
 		return redirect('/wordchef')
 	return render_template('recipe.html', title='word+chef', form=form)
 
