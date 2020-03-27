9bbe8cc03a61d5956de714b9552c25dc31a55469
Jackson Walters <jacksonwalters@gmail.com>
2020-03-27 01:42:30 -0400

    rename

--- similar.py
+++ similar.py
@@ -5,7 +5,7 @@ def cos_sim(vec1,vec2):
 	return 1-spatial.distance.cosine(vec1,vec2)
 
 #find most similar word given word vector
-def most_similar(word_vec):
-	queries = [w for w in nlp.vocab if w.is_lower and w.prob >= -15]
+def sim_words_from_vec(vocab,word_vec):
+	queries = [w for w in vocab if w.is_lower and w.prob >= -15]
 	by_similarity = sorted(queries, key=lambda w: cos_sim(w.vector,word_vec), reverse=True)
 	return by_similarity[:10]
