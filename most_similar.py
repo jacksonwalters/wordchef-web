016a208b1db491849081fd8d17ef313f8ea1b2b8
Jackson Walters <jacksonwalters@gmail.com>
2020-03-27 01:32:48 -0400

    use nlp object for vocab context

--- most_similar.py
+++ most_similar.py
@@ -1,5 +1,11 @@
-#find words similar/close to wordvector
-def most_similar(word,word_vec):
-	queries = [w for w in word.vocab if w.is_lower == word.is_lower and w.prob >= -15]
-	by_similarity = sorted(queries, key=lambda w: word.similarity(w), reverse=True)
+from scipy import spatial
+
+#cosine similarity of two vectors
+def cos_sim(vec1,vec2):
+	return 1-spatial.distance.cosine(vec1,vec2)
+
+#find most similar word given word vector
+def most_similar(word_vec):
+	queries = [w for w in nlp.vocab if w.is_lower and w.prob >= -15]
+	by_similarity = sorted(queries, key=lambda w: cos_sim(w.vector,word_vec), reverse=True)
 	return by_similarity[:10]
