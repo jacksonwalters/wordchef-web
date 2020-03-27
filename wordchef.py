daef15f1dbf183f850c8982660c566846848e4a0
Jackson Walters <jacksonwalters@gmail.com>
2020-03-27 00:52:29 -0400

    adding word vectors

--- wordchef.py
+++ wordchef.py
@@ -9,9 +9,18 @@ nlp = spacy.load('en_core_web_sm')
 def recipe():
 	form = RecipeForm()
 	if form.validate_on_submit():
-		vec1 = nlp(form.word1.data)
-		vec2 = nlp(form.word2.data)
-		flash('word1={}, word2={}'.format(form.word1.data, form.word2.data))
+		#retrieve words
+		word1 = form.word1.data
+		word2 = form.word2.data
+		#use spacy to get word vectors
+		vec1 = nlp(form.word1.data).vector
+		vec2 = nlp(form.word2.data).vector
+		#add the vectors
+		vec_sum = vec1 + vec2
+		#look up synonym from vector sum
+		#for now, just return sum as string
+		sum_word = ','.join(vec_sum.astype(str))
+		flash('{}+{}={}'.format(word1, word2, sum_word))
 		return redirect('/wordchef')
 	return render_template('recipe.html', title='word+chef', form=form)
 
