4702a78f4b3e59fcfa8f96490fbd8e635025ad45
Jackson Walters <jacksonwalters@gmail.com>
2020-03-30 23:51:18 -0400

    search low prob words
    allow lower probability words

--- similar.py
+++ similar.py
@@ -6,7 +6,8 @@ def cos_sim(vec1,vec2):
 
 #find most similar word given word vector
 def sim_words_from_vec(vocab,word_vec):
-	queries = [w for w in vocab if w.is_lower and w.prob >= -15]
+	#prob controls number of queries to search. -15=32k, -18=147k, -20=563k.
+	queries = [w for w in vocab if w.is_lower and w.prob >= -18]
 	by_similarity = sorted(queries, key=lambda w: cos_sim(w.vector,word_vec), reverse=True)
 	sim_tokens = by_similarity[:10]
 	return [token.text for token in sim_tokens]
