4fbc12efdb7acea305ee4be4c48e2902e0bedcf0
Jackson Walters <jacksonwalters@gmail.com>
2020-03-30 23:53:10 -0400

    prob >= -16

--- similar.py
+++ similar.py
@@ -7,7 +7,7 @@ def cos_sim(vec1,vec2):
 #find most similar word given word vector
 def sim_words_from_vec(vocab,word_vec):
 	#prob controls number of queries to search. -15=32k, -18=147k, -20=563k.
-	queries = [w for w in vocab if w.is_lower and w.prob >= -18]
+	queries = [w for w in vocab if w.is_lower and w.prob >= -16]
 	by_similarity = sorted(queries, key=lambda w: cos_sim(w.vector,word_vec), reverse=True)
 	sim_tokens = by_similarity[:10]
 	return [token.text for token in sim_tokens]
