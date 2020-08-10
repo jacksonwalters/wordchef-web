c723b8e8a10bcac49003deee6fa6dbfea81e31d3
Jackson Walters <jacksonwalters@gmail.com>
2020-08-10 16:47:41 -0400

    lookup file using balltrees
    lookup file which uses balltrees to speed up the search

---
+++ lookup.py
@@ -0,0 +1,48 @@
+import numpy
+import sklearn
+import pickle
+
+#compute linear combination amounts*vec(words). find nearest neighbor words.
+def nearest_words(amounts,words):
+	#load {word:vector} dictionary from pickle
+	with open('dict.pkl') as f:
+		to_vec = pickle.load(f)
+
+	#get vectors for all words
+	vecs=[]
+	for word in words:
+		try:
+			vec = to_vec[word]
+			vecs.append(vec)
+		#if word is not in vocab, do not include
+		except KeyError:
+			vecs.append(numpy.zeros(300))
+	
+	#clear dictionary from memory
+	to_vec = None
+
+	#compute linear combination of user wordvectors
+	assert len(vecs) == len(amounts)
+	n = len(vecs)
+	lin_comb = numpy.zeros(300)
+	for i in range(n):
+		lin_comb += amounts[i]*vecs[i]
+	
+
+	#load wordvector balltree from pickle file 
+	with open('balltree.pkl') as f:
+		tree = pickle.load(f)
+	
+	#perform nearest neighbors search of 
+	dist, ind = tree.query([lin_comb],5)
+	
+	#clear tree from memory
+	tree = None
+
+	#load vocab from pickle file
+	with open('words.pkl') as f:
+		vocab = pickle.load(f)
+
+	
+	
+	
