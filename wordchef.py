0a013c364b00a543fac9396053df7d8264abe249
jacksonwalters <jacksonwalters@gmail.com>
2021-02-05 12:25:30 -0500

    load vocab into memory once. move lookup function to main file

--- wordchef.py
+++ wordchef.py
@@ -1,17 +1,47 @@
-import logging
-from lookup import nearest_words
+import logging, pickle, numpy
 from flask import Flask, render_template, flash, redirect
 from forms import RecipeForm
 
+#initialize flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'play-it-as-it-lays'
 
-#if not run directly, enable gunicorn level logging
-if __name__ != '__main__':
-	#set loggers to gunicorn
-	gunicorn_logger = logging.getLogger('gunicorn.error')
-	app.logger.handlers = gunicorn_logger.handlers
-	app.logger.setLevel(gunicorn_logger.level)
+#load word2vec dictionary from pickle file
+with open('./vocab/dict.pkl','rb') as f:
+	to_vec = pickle.load(f)
+#load wordvector balltree from pickle file
+with open('./vocab/balltree.pkl','rb') as f:
+	tree = pickle.load(f)
+#load vocab list from pickle file
+with open('./vocab/vocab.pkl','rb') as f:
+	vocab = pickle.load(f)
+
+#form linear combination of wordvectors and lookup nearest words
+def nearest_words(amounts,words):
+	#get vectors for all words
+	vecs=[]
+	for word in words:
+		try:
+			vec = to_vec[word]
+			vecs.append(vec)
+		#if word is not in vocab, do not include
+		except KeyError:
+			vecs.append(numpy.zeros(300))
+
+	#compute linear combination of user wordvectors
+	assert len(vecs) == len(amounts)
+	n = len(vecs)
+	lin_comb = numpy.zeros(300)
+	for i in range(n):
+		lin_comb += amounts[i]*vecs[i]
+
+	#perform nearest neighbor search of wordvector vocabulary
+	dist, ind = tree.query([lin_comb],10)
+
+	#lookup nearest words using indices from tree
+	near_words = [vocab[i] for i in ind[0]]
+
+	return near_words
 
 @app.route("/", methods=['GET','POST'])
 def recipe():
@@ -25,10 +55,10 @@ def recipe():
 		amounts = [amount1,amount2,amount3]
 
 		#retrieve word data from form. ""->" " if (optionally) empty
-		word1 = (" " if form.word1.data == "" else form.word1.data)
-		word2 = (" " if form.word2.data == "" else form.word2.data)
-		word3 = (" " if form.word3.data == "" else form.word3.data)
-		words = [word1,word2,amount3]
+		word1 = (" " if form.word1.data == "" else form.word1.data.strip().lower())
+		word2 = (" " if form.word2.data == "" else form.word2.data.strip().lower())
+		word3 = (" " if form.word3.data == "" else form.word3.data.strip().lower())
+		words = [word1,word2,word3]
 
 		#look up synonyms from vector sum
 		result = ','.join(nearest_words(amounts,words))
@@ -40,3 +70,8 @@ def recipe():
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
+else:
+	#enable gunicorn level logging
+	gunicorn_logger = logging.getLogger('gunicorn.error')
+	app.logger.handlers = gunicorn_logger.handlers
+	app.logger.setLevel(gunicorn_logger.level)
