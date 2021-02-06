1e2d762fcb2a9a081a803a87a64d7767c6cc5da4
jacksonwalters <jacksonwalters@gmail.com>
2021-02-06 14:37:25 -0500

    updated gen_vocab_vecs.py
    spaCy changed to v3.0. it now uses nlp.vocab.strings and nlp.vocab.vectors to store words and wordvectors. removed the file splitting, which is easy and will write if needed,

--- gen_vocab_vecs.py
+++ gen_vocab_vecs.py
@@ -5,60 +5,43 @@ import pickle
 import random
 import sys
 
+#load NLP tool spaCy
 print("Loading spaCy...")
 nlp=spacy.load("en_core_web_lg")
 print("spaCy loaded.")
 
 #get plaintext words as list from spacy vocab. ensure they have wordvector, are lowercase, and aren't too rare
 #{prob:# words} ~ {-15:32k, -16:50k, -17:77k, -18:147k, -19:183k, -20:302k}
-print("Getting words...") 
-words = [w.text for w in nlp.vocab if w.is_lower and w.prob >= -20 and w.has_vector]
+print("Total number of words=",len(nlp.vocab.strings))
+print("Getting words...")
+words = [word for word in nlp.vocab.strings if nlp.vocab.has_vector(word) and word.islower()]
+print("Retrieved ",len(words),"words with vectors.")
 
 #get wordvectors for all words as numpy array
+print("Total number of wordvectors=",len(nlp.vocab.vectors))
 print("Getting wordvectors...")
-wordvecs = numpy.array([w.vector for w in nlp.vocab if w.is_lower and w.prob >= -20 and w.has_vector])
+wordvecs = numpy.array([nlp.vocab.get_vector(word) for word in words])
+print("Retrieved=",len(wordvecs),"wordvectors.")
 
 #ensure the list of words corresponds to the list of wordvectors
 assert len(words) == len(wordvecs)
 spot_check = random.choice(range(0,len(words)))
 assert numpy.array_equal(nlp(words[spot_check]).vector,wordvecs[spot_check])
+print("Spot check passed.")
 
 #pickle the entire vocab
 with open('words.pkl', 'wb') as f:
 		pickle.dump(words,f,protocol=pickle.HIGHEST_PROTOCOL)
+print("Dumped vocab words to pickle file vocab.pkl")
 
 #place all wordvectors in balltree, and pickle entire tree
 tree = nbs.BallTree(wordvecs)
-with open('wordvecs.pkl', 'wb') as f:
+with open('balltree.pkl', 'wb') as f:
 		pickle.dump(tree,f,protocol=pickle.HIGHEST_PROTOCOL)
+print("Dumped wordvector BallTree to pickle file balltree.pkl")
 
-#split words into separate files of ~22k words/wordvectors each
-#~17 bytes/word, as each word is at most ~17 characters
-#~1.2 mB/vector, as each vector is 300 rational numbers
-num_files = int(sys.argv[1])
-file_len = len(words)//num_files
-for i in range(0,num_files):
-	print("Writing file #"+str(i)+"...")
-	words_i = words[i*file_len:(i+1)*file_len]
-	wordvecs_i = wordvecs[i*file_len:(i+1)*file_len]
-	tree_i = nbs.BallTree(wordvecs_i)
-	with open('words_'+str(i)+'.pkl', 'wb') as f:
-		pickle.dump(words_i,f,protocol=pickle.HIGHEST_PROTOCOL)
-	with open('wordvecs_'+str(i)+'.pkl', 'wb') as f:
-		pickle.dump(tree_i,f,protocol=pickle.HIGHEST_PROTOCOL)
-	
-#if number of files does not evenly divide file length, append final file
-if len(words) % num_files != 0:
-	last_ind = num_files
-	print("Writing file #"+str(last_ind)+"...")
-	last_words = words[num_files*file_len:num_files*file_len+len(words)%num_files]
-	last_wordvecs = wordvecs[num_files*file_len:num_files*file_len+len(words)%num_files]
-	last_tree = nbs.BallTree(last_wordvecs)
-	with open('words_'+str(last_ind)+'.pkl', 'wb') as f:
-		pickle.dump(last_words,f,protocol=pickle.HIGHEST_PROTOCOL)
-	with open('wordvecs_'+str(last_ind)+'.pkl', 'wb') as f:
-		pickle.dump(last_tree,f,protocol=pickle.HIGHEST_PROTOCOL)
-	
-	
-	
-
+#place all wordvectors in balltree, and pickle entire tree
+dict = dict(zip(words,wordvecs))
+with open('dict.pkl', 'wb') as f:
+		pickle.dump(dict,f,protocol=pickle.HIGHEST_PROTOCOL)
+print("Dumped wordvector BallTree to pickle file wordvecs.pkl")
