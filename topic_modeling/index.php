df73b2f017da89570f730aa473322b5502feb75f
Jackson Walters <jacksonwalters@gmail.com>
2024-09-17 18:52:19 -0400

    begin webapp for topic modeling

---
+++ topic_modeling/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Topic Modeling</title>
+</head>
+<body>
+<h1>Topic Modeling</h1>
+<form action="index.php" method="POST" enctype="multipart/form-data">
+    <input type="file" name="csv_file" accept=".csv">
+    <button type="submit">Upload</button>
+</form>
+</body>
+</html>
